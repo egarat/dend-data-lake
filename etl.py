b27895fecd6eecc61daa73f8ae58ca664d237db1
Egarat Kaoroprat <egarat.kaoroprat@gmail.com>
2021-05-22 21:54:01 +0200

    Add process for loading artists table

--- etl.py
+++ etl.py
@@ -35,10 +35,10 @@ def process_song_data(spark, input_data, output_data):
     songs_table.write.mode("overwrite").partitionBy("year", "artist_id").parquet(f"{output_data}songs")
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = df.selectExpr("artist_id", "artist_name AS name", "artist_location AS location", "artist_latitude AS latitude", "artist_longitude AS longitude").dropDuplicates(["artist_id"])
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.mode("overwrite").parquet(f"{output_data}artists")
 
 
 def process_log_data(spark, input_data, output_data):
@@ -52,30 +52,30 @@ def process_log_data(spark, input_data, output_data):
     df = df.where(df["page"] == "NextSong")
 
     # extract columns for users table    
-    artists_table = 
+    users_table = ""
     
     # write users table to parquet files
-    artists_table
+    users_table
 
     # create timestamp column from original timestamp column
     get_timestamp = udf()
-    df = 
+    df = ""
     
     # create datetime column from original timestamp column
     get_datetime = udf()
-    df = 
+    df = ""
     
     # extract columns to create time table
-    time_table = 
+    time_table = ""
     
     # write time table to parquet files partitioned by year and month
     time_table
 
     # read in song data to use for songplays table
-    song_df = 
+    song_df = ""
 
     # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    songplays_table = ""
 
     # write songplays table to parquet files partitioned by year and month
     songplays_table
